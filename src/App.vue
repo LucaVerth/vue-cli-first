1fde32345684c41396ca0046cdcc357d37b0ce4a
LucaVerth <luca.verthuy@gmail.com>
2021-11-23 16:01:08 +0100

    Title done // working on Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    
+    <Title/>
   </div>
 </template>
 
 <script>
-
+import Title from "./components/Title.vue"
 
 export default {
   name: 'App',
   components: {
-    
+    Title
   }
 }
 </script>
 
 <style lang="scss">
-
+#app{
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header{
+  height: 70px;
+  background-color: royalblue;
+  border-radius: 20px;
+  line-height: 70px;
+}
+h1{
+  text-align: center;
+  color: red;
+  text-shadow: 0 10px 20px #000;
+}
+</style>
